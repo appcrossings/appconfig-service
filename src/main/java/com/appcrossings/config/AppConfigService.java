9bb28e10a9d7d39ceb03d1e0a356d6605f790fb0
Krzysztof Karski <kkarski@appcrossings.com>
2018-03-13 13:56:06 -0700

    fixed yaml, consolidated REST api

--- src/main/java/com/appcrossings/config/AppConfigService.java
+++ src/main/java/com/appcrossings/config/AppConfigService.java
@@ -7,24 +7,20 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/")
+@Path("/v1")
 public interface AppConfigService {
 
   @GET
-  @Path("/config/{path:.+}")
+  @Path("/{repo}/{path:.+}")
   @Consumes({MediaType.WILDCARD, MediaType.TEXT_PLAIN})
   @Produces({MediaType.TEXT_PLAIN, MediaType.APPLICATION_JSON, "application/x-yam"})
-  Response getRawProperties(@NotNull @PathParam("path") String path);
-  
-  @GET
-  @Path("/raw/{path:.+}")
-  @Consumes({MediaType.WILDCARD, MediaType.TEXT_PLAIN})
-  @Produces(MediaType.TEXT_PLAIN)
-  Response getResolvedProperties(@NotNull @PathParam("path") String path);
-  
+  Response resolveProperties(@NotNull @PathParam("repo") String repo,
+      @NotNull @PathParam("path") String path, @QueryParam("t") Boolean traverse);
+
   @GET
   @Path("/health")
   Response getHealth();
--- src/main/java/com/appcrossings/config/AppConfigServiceImpl.java
+++ src/main/java/com/appcrossings/config/AppConfigServiceImpl.java
@@ -1,16 +1,14 @@
 package com.appcrossings.config;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
-
 import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
-
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +16,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
-
+import com.appcrossings.config.strategy.DefaultMergeStrategy;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
@@ -30,22 +28,36 @@ public class AppConfigServiceImpl implements AppConfigService {
   private String filesystemRoot;
 
   private final ObjectMapper mapper = new ObjectMapper();
-  
+
   @Autowired
   private ConfigSourceResolver resolver;
 
   private final DefaultResourceLoader loader = new DefaultResourceLoader();
 
   @Override
-  public Response getRawProperties(String path) {
+  public Response resolveProperties(String repo, String path, Boolean traverse) {
 
     logger.debug("Requested path" + path);
-    Resource r = loader.getResource(filesystemRoot + "/" + path);
 
-    try {
+    ConfigSource source = resolver.resolveSource(path);
+    Properties props = new Properties();
+    try (ByteArrayOutputStream output = new ByteArrayOutputStream()) {
 
-      byte[] content = IOUtils.toByteArray(r.getInputStream());
-      return Response.status(Status.OK).entity(content).build();
+      if (traverse == null || !traverse) {
+
+        props = source.fetchConfig(path, Config.DEFAULT_PROPERTIES_FILE_NAME);
+        props.store(output, "");
+
+      } else {
+
+        // TODO: Make default properties file configurable
+        props = source.traverseConfigs(path, Config.DEFAULT_PROPERTIES_FILE_NAME,
+            new DefaultMergeStrategy());
+
+      }
+
+      props.store(output, "");
+      return Response.status(Status.OK).entity(output.toByteArray()).build();
 
     } catch (FileNotFoundException not) {
 
@@ -84,14 +96,5 @@ public class AppConfigServiceImpl implements AppConfigService {
       return Response.status(Status.INTERNAL_SERVER_ERROR).cacheControl(control).build();
 
     }
-  }
-
-  @Override
-  public Response getResolvedProperties(String path) {
-    
-    logger.debug("Requested path" + path);
-    resolver.resolveSource(path);
-    
-    return null;
   }
 }
