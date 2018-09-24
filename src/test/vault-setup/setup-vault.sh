0d79e24d268bb4231a91153ec037103431bcaa15
Krzysztof Karski <kkarski@appcrossings.com>
2018-09-23 19:40:41 -0700

    fixed paths

--- src/test/vault-setup/setup-vault.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-export VAULT_ADDR='http://127.0.0.1:8200'
-
-./vault auth enable userpass
-
-./vault write auth/userpass/users/test password=password policies=admins
-
-./vault policy write admins ./admin.hcl
